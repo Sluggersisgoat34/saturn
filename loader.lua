0510b7cc2df3c9077082447a5e7439ed0f838c2e
Sluggersisgoat34 <155809166+Sluggersisgoat34@users.noreply.github.com>
2026-06-11 17:31:08 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 13822889 then -- 🌳 Lumber Tycoon 2
-loadstring(game:HttpGet('https://test.com'))("")/
+loadstring(game:HttpGet('https://gist.githubusercontent.com/Sluggersisgoat34/1a0e99a093f7f9576c3815f11bfe8397/raw/213ffab80ae2fec4c0c44ce2805ca41df9dca9de/lt2.lua'))("")/
 end
